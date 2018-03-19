48e81abf1ab6f9d3c076c5086f75b3d4ae4ac39b
esipode <esipode@hotmail.com>
2018-03-19 10:05:12 -0600

    Update Handlers.cs

--- Assembly-CSharp/Handlers.cs
+++ Assembly-CSharp/Handlers.cs
@@ -206,5 +206,5 @@ namespace Modding
     /// <summary>
     /// Called when the player heals
     /// </summary>
-    public delegate int BeforeAddHealthHandler();
+    public delegate int BeforeAddHealthHandler(int amount);
 }
