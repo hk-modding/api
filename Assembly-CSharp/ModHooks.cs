7889b4e292ecdd04875e18fecf3090f89a3dfa6c
esipode <esipode@hotmail.com>
2018-03-19 09:10:27 -0600

    Update ModHooks.cs

--- Assembly-CSharp/ModHooks.cs
+++ Assembly-CSharp/ModHooks.cs
@@ -1033,7 +1033,7 @@ namespace Modding
         /// Called whenever the player heals
         /// </summary>
         /// <remarks>PlayerData.health</remarks>
-	internal int BeforeAddHealth()
+	internal int BeforeAddHealth(int amount)
 	{
             Logger.LogFine("[API] - BeforeAddHealth Invoked");
 
