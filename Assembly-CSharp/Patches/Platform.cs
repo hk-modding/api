f7f70e2cefc2998b682306492b18950f8184f6d6
5FiftySix6 <ybham6@gmail.com>
2018-08-24 23:49:31 -0400

    Fix saves methods and fix save name method

---
+++ Assembly-CSharp/Patches/Platform.cs
@@ -0,0 +1,23 @@
+﻿using MonoMod;
+
+#pragma warning disable 1591
+namespace Modding.Patches
+{
+    [MonoModPatch("global::Platform")]
+    public abstract class Platform : global::Platform
+    {
+        protected extern string orig_GetSaveSlotFileName(int slotIndex, SaveSlotFileNameUsage usage);
+
+        public static bool IsSaveSlotIndexValid(int slotIndex) => true;
+        
+        // ReSharper disable once UnusedMember.Global
+        protected string GetSaveSlotFileName(int slotIndex, SaveSlotFileNameUsage usage)
+        {
+            string saveFileName = ModHooks.Instance.GetSaveFileName(slotIndex);
+            ModHooks.ModLog("[API] - " + saveFileName);
+            return string.IsNullOrEmpty(saveFileName)
+                ? orig_GetSaveSlotFileName(slotIndex, usage) 
+                : saveFileName;
+        }
+    }
+}
