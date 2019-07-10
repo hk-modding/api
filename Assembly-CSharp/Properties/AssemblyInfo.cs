4f6694cce4c74b1db6dd0bc0a760556853bf74db
5FiftySix6 <ybham6@gmail.com>
2019-07-10 18:35:36 -0400

    Rework csproj, lower build times, cross-platform

--- Assembly-CSharp/Properties/AssemblyInfo.cs
+++ Assembly-CSharp/Properties/AssemblyInfo.cs
@@ -1,15 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Hollow Knight - Mod API Enabled")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Modding API")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1cacea6a-d114-46b1-8c47-f2f92062b844")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
