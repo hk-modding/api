59dab5b9dd93eedb44b39614ca99361f6a5ca004
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2020-12-22 13:24:29 +0100

    Update Assembly-CSharp/Patches/AudioPlayerOneShotSingle.cs
    Co-authored-by: Yusuf Bham <10470872+fifty-six@users.noreply.github.com>

--- Assembly-CSharp/Patches/AudioPlayerOneShotSingle.cs
+++ Assembly-CSharp/Patches/AudioPlayerOneShotSingle.cs
@@ -18,7 +18,7 @@ namespace Modding.Patches
             {
                 orig_DoPlayRandomClip();
             }
-            catch (NullReferenceException)
+            catch (NullReferenceException) when (ModLoader.Preloading)
             {}
         }
     }
