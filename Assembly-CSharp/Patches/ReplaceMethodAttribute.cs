23b180330c9bf5e63de78f625bb7ed6688357f1d
Seanpr <seanpr96@gmail.com>
2021-03-04 05:29:43 -0800

    ReplaceMethod cleanup

---
+++ Assembly-CSharp/Patches/ReplaceMethodAttribute.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod;
+using MonoMod.Cil;
+
+namespace Modding.Patches
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// MonoMod attribute for replacing a method call
+    /// </summary>
+    [MonoModCustomAttribute("ReplaceMethod")]
+    [UsedImplicitly]
+    internal class ReplaceMethodAttribute : Attribute
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Replace method call with alternate method call
+        /// </summary>
+        public ReplaceMethodAttribute(string type1, string method1, string[] params1, string type2, string method2, string[] params2) { }
+    }
+}
+
+namespace MonoMod
+{
+    public static partial class MonoModRules
+    {
+        private const BindingFlags FLAGS = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+
+        /// <summary>
+        /// Replace method call with alternate method call
+        /// </summary>
+        /// <param name="method">Method to be patched</param>
+        /// <param name="attrib">Attribute ReplaceWithNop</param>
+        [UsedImplicitly]
+        public static void ReplaceMethod(MethodDefinition method, CustomAttribute attrib)
+        {
+            MethodReference oldMethod = method.Module.ImportReference
+            (
+                Type.GetType((string)attrib.ConstructorArguments[0].Value).GetMethod
+                (
+                    (string)attrib.ConstructorArguments[1].Value,
+                    FLAGS,
+                    null,
+                    ((CustomAttributeArgument[])attrib.ConstructorArguments[2].Value)
+                        .Select(t => Type.GetType((string)t.Value)).ToArray(),
+                    null
+                )
+            );
+
+            MethodReference newMethod = method.Module.ImportReference
+            (
+                Type.GetType((string)attrib.ConstructorArguments[3].Value).GetMethod
+                (
+                    (string)attrib.ConstructorArguments[4].Value,
+                    FLAGS,
+                    null,
+                    ((CustomAttributeArgument[])attrib.ConstructorArguments[5].Value)
+                        .Select(t => Type.GetType((string)t.Value)).ToArray(),
+                    null
+                )
+            );
+
+            ILCursor il = new ILCursor(new ILContext(method));
+            while (il.TryGotoNext(i => i.MatchCallOrCallvirt(oldMethod)))
+            {
+                il.Remove();
+                il.Emit(newMethod.HasThis ? OpCodes.Callvirt : OpCodes.Call, newMethod);
+            }
+        }
+    }
+}
