26d8e1bc30c2795f6442240481d914129523c08d
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2021-03-24 22:40:15 +0100

    Remove `betaEnd` PlayerData check

---
+++ Assembly-CSharp/Patches/HeroAnimationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using GlobalEnums;
+using MonoMod;
+using UnityEngine;
+
+// ReSharper disable All
+#pragma warning disable 1591, 0108, 0169, 0649, 0626, 414, CS0626
+
+namespace Modding.Patches
+{
+    [MonoModPatch("global::HeroAnimationController")]
+    public class HeroAnimationController : global::HeroAnimationController
+    {
+        [MonoModIgnore]
+        private HeroControllerStates cState;
+
+        [MonoModIgnore]
+        private bool wasFacingRight;
+
+        [MonoModIgnore]
+        private extern void UpdateAnimation();
+
+        [MonoModReplace]
+        private void Update()
+        {
+            if (this.controlEnabled)
+            {
+                this.UpdateAnimation();
+            }
+            else if (this.cState.facingRight)
+            {
+                this.wasFacingRight = true;
+            }
+            else
+            {
+                this.wasFacingRight = false;
+            }
+        }
+    }
+}
