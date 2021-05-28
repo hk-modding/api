3b2d405eb9a1c3012e084012dca8bbaeb32abb05
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2021-05-28 00:36:29 -0400

    Start updating the example mods and readmes The following examples have been added: 1. Super basic mod that demonstrates hooks 2. Mod that demonstrates custom save data

--- ExampleMods/README.md
+++
@@ -1,8 +0,0 @@
-Readme
-============
-
-Here is a collection of very simple example mods to get you started with writing your own mods.
-
-Each example works similar to the other, but shows how to have mods with no settings, save game settings, and global settings.  Each example also has helper code to package up your mod into the format that is expected for mods during the build event.
-
-To get started, download one of the example mods, open it in visual studio and click Build.  What you should end up with is a folder called Distribution with the built mod and also a zip file ready to be uploaded. 
---
+++ Examples/CustomSaveData/CustomSaveData.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using Modding;
+using System.Reflection;
+
+namespace ExampleMods
+{
+    // Define a new mod named `CustomSaveData` that implements `GlobalSettings`
+    // to signify that it will save some data to the saves folder.
+    public class CustomSaveData : Mod, GlobalSettings<CustomGlobalSaveData>, LocalSettings<CustomLocalSaveData>
+    {
+        public static CustomSaveData loadedInstance { get; set; }
+
+        // The global settings for this mod. The settings load will only occur once
+        // so a static field should be used to prevent loss of data
+        public static CustomGlobalSaveData globalSaveData { get; set; }
+        // Implement the GlobalSettings interface.
+        // This method gets called when the mod loader loads the global settings.
+        public void OnLoadGlobal(CustomGlobalSaveData s) => CustomSaveData.globalSaveData = s;
+        // This method gets called when the mod loader needs to save the global settings.
+        public CustomGlobalSaveData OnSaveGlobal() => CustomSaveData.globalSaveData;
+
+        // The save data specific to a certain savefile. This setting will be loaded each time a save is opened.
+        public CustomLocalSaveData localSaveData { get; set; }
+        // Implement the LocalSettings interface.
+        // This method gets called when a save is loaded.
+        public void OnLoadLocal(CustomLocalSaveData s) => this.localSaveData = s;
+        // This method gets called when the player saves their file.
+        public CustomLocalSaveData OnSaveLocal() => this.localSaveData;
+
+        public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        // The global settings are loaded before this method is called.
+        public override void Initialize()
+        {
+            if (CustomSaveData.loadedInstance != null) return;
+            CustomSaveData.loadedInstance = this;
+            // Hook into the savegame load hook (which will occur after OnLoadLocal is called).
+            ModHooks.SavegameLoadHook += slot =>
+            {
+                this.localSaveData.loaded += 1;
+                CustomSaveData.globalSaveData.savesLoaded += 1;
+                this.Log($"Save {slot} has been loaded {this.localSaveData.loaded} times!");
+                this.Log($"Saves have been loaded a total of {CustomSaveData.globalSaveData.savesLoaded} times!");
+            };
+        }
+    }
+
+    // These do not have to be classes, the mod loader can serialize and deserialize value types as well.
+    // The global data to store that is transient between saves.
+    public class CustomGlobalSaveData
+    {
+        // The number of times the player has loaded into a save.
+        public int savesLoaded;
+    }
+
+    // The local data to store that is specific to saves.
+    public class CustomLocalSaveData
+    {
+        // The number of times the player has loaded into this save.
+        public int loaded;
+    }
+}
