7ff19414ff16d120420e97ee5bf8c81222e92d0e
Mandar1jn <marijnkneppers@gmail.com>
2021-06-09 19:13:38 +0200

    removed unnecessary surpression

--- Assembly-CSharp/GlobalSuppressions.cs
+++ Assembly-CSharp/GlobalSuppressions.cs
@@ -6,12 +6,8 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.GameManager.orig_OnApplicationQuit")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.GameManager.orig_LoadScene(System.String)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.GameManager.orig_GetSaveFilename(System.Int32)~System.String")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.GameManager.orig_ClearSaveFile(System.Int32)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.HeroController.orig_TakeDamage(UnityEngine.GameObject,GlobalEnums.CollisionSide,System.Int32,System.Int32)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.HeroController.orig_StartMPDrain(System.Single)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.PlayerData.orig_TakeHealth(System.Int32)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.PlayerData.orig_UpdateBlueHealth")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Modding.Patches.PlayerData.orig_SetupNewPlayerData")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Modding.Patches.PlayerData.instance")]
 
