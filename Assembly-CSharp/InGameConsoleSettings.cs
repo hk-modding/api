c682c99aa96bf3d19ef30bdc6681eb241fbea432
Mandar1jn <marijnkneppers@gmail.com>
2021-07-01 10:07:04 +0200

    moved settings related to the colors of the in game console to a seperate class and settings file

--- Assembly-CSharp/InGameConsoleSettings.cs
+++ Assembly-CSharp/InGameConsoleSettings.cs
@@ -1,4 +1,10 @@
-﻿namespace Modding
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modding
 {
     /// <summary>
     ///	    Settins related to the in-game console
