9ce86094cadcb3dab408b9a917434eae026ea193
Mandar1jn <marijnkneppers@gmail.com>
2021-07-01 10:07:04 +0200

    moved settings related to the colors of the in game console to a seperate class and settings file

---
+++ Assembly-CSharp/InGameConsoleSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modding
+{
+    /// <summary>
+    ///		
+    /// </summary>
+    public class InGameConsoleSettings
+    {
+        /// <summary>
+        ///	    Wheter to use colors in the log console.
+        /// </summary>
+        public bool UseLogColors;
+
+        /// <summary>
+        ///     The color to use for Fine logging when UseLogColors is enabled
+        /// </summary>
+        public string FineColor = "grey";
+
+        /// <summary>
+        ///	    The color to use for Info logging when UseLogColors is enabled
+        /// </summary>
+        public string InfoColor = "cyan";
+
+        /// <summary>
+        ///	    The color to use for Debug logging when UseLogColors is enabled
+        /// </summary>
+        public string DebugColor = "white";
+
+        /// <summary>
+        ///	    The color to use for Warning logging when UseLogColors is enabled
+        /// </summary>
+        public string WarningColor = "yellow";
+
+        /// <summary>
+        ///	    The color to use for Error logging when UseLogColors is enabled
+        /// </summary>
+        public string ErrorColor = "red";
+
+        /// <summary>
+        ///	    The color to use when UseLogColors is disabled
+        /// </summary>
+        public string DefaultColor = "white";
+    }
+}
