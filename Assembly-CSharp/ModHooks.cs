078905e58972e3fe693abef750e1965100733853
fifty-six <ybham6@gmail.com>
2021-08-30 17:51:38 -0400

    Bump version

--- Assembly-CSharp/ModHooks.cs
+++ Assembly-CSharp/ModHooks.cs
@@ -24,7 +24,7 @@ namespace Modding
     [PublicAPI]
     public class ModHooks
     {
-        private const int _modVersion = 62;
+        private const int _modVersion = 63;
 
         internal static bool IsInitialized;
 
