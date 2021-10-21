15a3b6b1c95f7ff5007a010ac6cd5c2ee0fb1f28
fifty-six <ybham6@gmail.com>
2021-10-21 02:38:13 -0400

    Update API to 1.5.78, v65

--- Assembly-CSharp/ModHooks.cs
+++ Assembly-CSharp/ModHooks.cs
@@ -24,7 +24,7 @@ namespace Modding
     [PublicAPI]
     public class ModHooks
     {
-        private const int _modVersion = 64;
+        private const int _modVersion = 65;
 
         internal static bool IsInitialized;
 
