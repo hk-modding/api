e75272cec226a4caa33fd732b1fc7a68bf24b64a
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-27 10:00:49 +0000

    Revert back to Log (info level)
    While it probably fits better in debug, the intended use case is for dealing with bug/support reports from users, so switching to debug log level to get the more informative logs isn't really viable.

--- Assembly-CSharp/ModLoader.cs
+++ Assembly-CSharp/ModLoader.cs
@@ -237,7 +237,7 @@ namespace Modding
             UpdateModText();
 
             // Adding version nums to the modlog by default to make debugging significantly easier
-            Logger.APILogger.LogDebug("Set mod text:\n" + modVersionDraw.drawString);
+            Logger.APILogger.Log("Finished loading mods:\n" + modVersionDraw.drawString);
 
             ModHooks.OnFinishedLoadingMods();
             Loaded = true;
