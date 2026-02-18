b74e35b409309d202103b9dce5c944265395e2b2
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-02-18 22:29:31 +0100

    make comment clear make compat methods private

--- Assembly-CSharp/Language/Language.cs
+++ Assembly-CSharp/Language/Language.cs
@@ -27,51 +27,52 @@ public static class Language
     public static string GetInternal(string key, string sheetTitle) => COMPAT_Get(key, sheetTitle);
     public static string Get(string key, string sheetTitle) => Modding.ModHooks.LanguageGet(key, sheetTitle);
 
-    // for backwards compatibility
-    // KEEP THIS BELOW THE Language CLASS!!!
+    // Keep these below the `[MonoMod.MonoModLinkFrom("TeamCherry.Localization.Language")]`, as the reverse order would cause a cyclic loop of methods calling themselves
+    // thanks to MonoMod resolving these links top-down
+    // which would make a `LinkTo(TCLL)->LinkFrom(TCLL)` into a fully recursive function ⟳
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Void LoadLanguage()")]
     [MonoMod.MonoModRemove]
-    public static extern void COMPAT_LoadLanguage();
+    private static extern void COMPAT_LoadLanguage();
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Void LoadAvailableLanguages()")]
     [MonoMod.MonoModRemove]
-    public static extern void COMPAT_LoadAvailableLanguages();
+    private static extern void COMPAT_LoadAvailableLanguages();
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.String[] GetLanguages()")]
     [MonoMod.MonoModRemove]
-    public static extern string[] COMPAT_GetLanguages();
+    private static extern string[] COMPAT_GetLanguages();
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Boolean SwitchLanguage(System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern bool COMPAT_SwitchLanguage(string langCode);
+    private static extern bool COMPAT_SwitchLanguage(string langCode);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Boolean SwitchLanguage(TeamCherry.Localization.LanguageCode)")]
     [MonoMod.MonoModRemove]
-    public static extern bool COMPAT_SwitchLanguage(TeamCherry.Localization.LanguageCode code);
+    private static extern bool COMPAT_SwitchLanguage(TeamCherry.Localization.LanguageCode code);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "UnityEngine.Object GetAsset(System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern UObject COMPAT_GetAsset(string name);
+    private static extern UObject COMPAT_GetAsset(string name);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "TeamCherry.Localization.LanguageCode CurrentLanguage()")]
     [MonoMod.MonoModRemove]
-    public static extern TeamCherry.Localization.LanguageCode COMPAT_CurrentLanguage();
+    private static extern TeamCherry.Localization.LanguageCode COMPAT_CurrentLanguage();
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.String Get(System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern string COMPAT_Get(string key);
+    private static extern string COMPAT_Get(string key);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.String Get(System.String,System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern string COMPAT_Get(string key, string sheetTitle);
+    private static extern string COMPAT_Get(string key, string sheetTitle);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Collections.Generic.IEnumerable`1<System.String> GetSheets()")]
     [MonoMod.MonoModRemove]
-    public static extern IEnumerable<string> COMPAT_GetSheets();
+    private static extern IEnumerable<string> COMPAT_GetSheets();
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Collections.Generic.IEnumerable`1<System.String> GetKeys(System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern IEnumerable<string> COMPAT_GetKeys(string sheetTitle);
+    private static extern IEnumerable<string> COMPAT_GetKeys(string sheetTitle);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Boolean Has(System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern bool COMPAT_Has(string key);
+    private static extern bool COMPAT_Has(string key);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Boolean Has(System.String,System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern bool COMPAT_Has(string key, string sheet);
+    private static extern bool COMPAT_Has(string key, string sheet);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "System.Boolean HasSheet(System.String)")]
     [MonoMod.MonoModRemove]
-    public static extern bool COMPAT_HasSheet(string sheet);
+    private static extern bool COMPAT_HasSheet(string sheet);
     [MonoMod.MonoModLinkTo("TeamCherry.Localization.Language", "TeamCherry.Localization.LanguageCode LanguageNameToCode(UnityEngine.SystemLanguage)")]
     [MonoMod.MonoModRemove]
-    public static extern TeamCherry.Localization.LanguageCode COMPAT_LanguageNameToCode(USystemLanguage name);
+    private static extern TeamCherry.Localization.LanguageCode COMPAT_LanguageNameToCode(USystemLanguage name);
 }
